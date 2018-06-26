37a9676357a72536cda6a307c1327fee682ce755
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-06-26 11:09:56 +0300

    Run on production mode on.Make app accessible to external devices.

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from application import create_app
 
 
 if __name__ == '__main__':
     app = create_app('default')
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0')
