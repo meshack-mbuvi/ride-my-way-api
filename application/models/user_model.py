cb763fd64fba8cb702a647d3b8804ad9dadc3a6a
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-06-27 22:43:50 +0300

    [Feature #158518839]Add model for user. Add a view function for user signup and within it implement the functionality to create user account.

---
+++ application/models/user_model.py
@@ -0,0 +1,39 @@
+from validate_email import validate_email
+from werkzeug.security import generate_password_hash
+
+from . import *
+
+
+def createTable(table_name):
+    cur = con.cursor()
+    try:
+        cur.execute('SELECT 1 from {}' . format(table_name))
+        return True
+    except Exception as e:
+        commands = 'CREATE TABLE users (user_id serial PRIMARY KEY, \
+                        username varchar(255), \
+                        email varchar(50) NOT NULL, \
+                        password varchar(255) NOT NULL, \
+                        phone varchar(255) NOT NULL, driver boolean )'
+        cur.execute(commands)
+
+        return False
+
+
+class User():
+
+    def __init__(self, user_data):
+        self.username = user_data['username']
+        self.email = user_data['email']
+        self.password = generate_password_hash(
+            user_data['password'], method='sha256')
+        self.phone = user_data['phone']
+        self.driver = user_data['driver']
+
+    def save(self):
+        # insert new record
+        query = "INSERT INTO users (username,email,password,phone,driver) VALUES " \
+            "('" + self.username + "', '" + self.email + "', '" + self.password + "', \
+             {},{})". format(self.phone, self.driver)
+        cur.execute(query)
+        con.commit()
