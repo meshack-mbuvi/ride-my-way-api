a1137fc2888ab156e36b6faf2d122ba0f9b76ac1
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-06-28 11:02:19 +0300

    [Feature #158684349] Update configuration for app to use JWT.

--- instance/config.py
+++ instance/config.py
@@ -5,6 +5,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
+    CSRF_ENABLED = True
+    JWT_ALGORITHM = 'HS256'
+    JWT_SECRET_KEY = 'You and me knows very well it is secret'
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
 
 
 class ProductionConfig(Config):
