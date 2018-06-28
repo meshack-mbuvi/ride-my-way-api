c6c153d452722686a615efbf4506ba531edaafb7
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-06-28 14:22:30 +0300

    [Feature #158628185] Add helper modules for creating new ride offer.

--- application/manage.py
+++ application/manage.py
@@ -6,17 +6,16 @@ host = 'localhost'
 password = 'ridemyway'
 
 
-class db():
+class database():
 
-    def connect(self, dbname):
-        dbname = dbname
-        con = connect(database=dbname, user=user, host=host, password=password)
-        con.autocommit = True
-        return con
+    def connect(self):
+        connection = connect(database=dbname, user=user, host=host, password=password)
+        connection.autocommit = True
+        return connection
 
-    def create_all(self, dbname=dbname):
+    def create_all(self):
         # Create all tables here
-        con = self.connect(dbname)
+        connection = self.connect()
         commands = (
             'DROP TABLE "users" CASCADE',
             'CREATE TABLE users (user_id serial PRIMARY KEY, \
@@ -28,53 +27,54 @@ class db():
             'DROP TABLE "rides" CASCADE',
             'CREATE TABLE rides (ride_id serial PRIMARY KEY, \
                        owner_id serial, \
+                       startPoint varchar(255), \
                        destination varchar(255), \
-                       start_time varchar(50) NOT NULL, \
+                       startTime varchar(50) NOT NULL, \
                        route varchar(255) NOT NULL, \
-                       available_space Int NOT NULL, \
+                       availablePpace Int NOT NULL, \
                        FOREIGN KEY (owner_id) REFERENCES users(user_id) )'
         )
 
         try:
-            cur = con.cursor()
+            cursor = connection.cursor()
             # create table one by one
             print("Creating relations")
             for command in commands:
-                cur.execute(command)
+                cursor.execute(command)
             # close communication with the PostgreSQL database server
-            cur.close()
+            cursor.close()
             # commit the changes
-            con.commit()
+            connection.commit()
             print("Done.")
         except (Exception) as error:
             print(error)
         finally:
-            if con is not None:
-                con.close()
+            if connection is not None:
+                connection.close()
 
-    def drop_all(self, dbname=dbname):
-        con = self.connect(dbname)
+    def drop_all(self):
+        connection = self.connect()
         commands = (
             'DROP TABLE "users" CASCADE',
             'DROP TABLE "rides" CASCADE')
         try:
-            con.autocommit = True
-            cur = con.cursor()
+            connection.autocommit = True
+            cursor = connection.cursor()
             # create table one by one
             print("Deleting relations")
             for command in commands:
-                cur.execute(command)
+                cursor.execute(command)
             # close communication with the PostgreSQL database server
-            cur.close()
+            cursor.close()
             # commit the changes
-            con.commit()
+            connection.commit()
             print("Done.")
         except (Exception) as error:
             print(error)
         finally:
-            if con is not None:
-                con.close()
+            if connection is not None:
+                connection.close()
 
 if __name__ == '__main__':
-    db = db()
-    db.create_all()
+    dbObject = database()
+    dbObject.create_all()
