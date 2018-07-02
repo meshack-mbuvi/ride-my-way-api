c0e7db4ba2731f31221d18a4274c2c5b8500dbe7
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-07-02 05:22:46 +0300

    [Feature #158751731] Update/reset user password. [Delivers #158751731]

--- tests/testRides.py
+++ tests/testRides.py
@@ -166,21 +166,6 @@ class RidesOfferTests(unittest.TestCase):
         response_data = json.loads(response.get_data().decode('utf-8'))
         self.assertTrue(response_data[0] is not None)
 
-    def test_user_can_request_a_ride(self):
-        """test user can join a ride."""
-        # create a ride to be sure a ride exists.
-        response = self.app.post('/api/v1/users/rides',
-                                 data=json.dumps(self.ride),
-                                 content_type='application/json',
-                                 headers=self.headers)
-        response = self.app.post('/api/v1/rides/1/requests',
-                                 content_type='application/json',
-                                 headers=self.headers)
-        response_data = json.loads(response.get_data().decode('utf-8'))
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response_data['message'],
-                         "Your request has been send.")
-
     def test_user_cannot_request_a_non_existing_ride(self):
         """test user cannot request a non existing ride offer."""
         response = self.app.post('/api/v1/rides/-1/requests',
