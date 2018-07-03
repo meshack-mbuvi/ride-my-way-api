cbf18badf744710b68e3b3cda087dfd3bb4c0a9b
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-07-03 14:40:00 +0300

    Configure database for heroku

--- application/models/__init__.py
+++ application/models/__init__.py
@@ -6,7 +6,7 @@ host = 'ec2-54-225-230-142.compute-1.amazonaws.com'
 user = 'fykazngytmidee'
 password = '7a940a85b94644e69d871928b9dc8a7b1dda264fcfb4724ca6c0f423514b230b'
 database = 'du15ldvvdve7g'
-connection = connect(database=dbname, user=user, host=host, password=password)
+connection = connect(database=database, user=user, host=host, password=password)
 connection.autocommit = True
 cursor = connection.cursor()
 
