fbcbc677bfd5968118edd4ecb7b5dff81263024b
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-07-31 20:56:04 +0300

    Fix failing tests on deleting user request.

--- application/views/ride_views.py
+++ application/views/ride_views.py
@@ -391,17 +391,17 @@ class Requests(Resource):
         except Exception as e: return e, 500
 
     @jwt_required
-    def delete(self, ride_id):
+    def delete(self, request_id):
         query = "select user_id from users where email='{}'".format(get_jwt_identity())
         result = db.execute(query)
         user_id = result.fetchone()
 
-        query = "select * from requests where ride_id='{}' and user_id='{}'"\
-                .format(ride_id, user_id[0])
+        query = "select * from requests where req_id='{}' and user_id='{}'"\
+                .format(request_id, user_id[0])
         result = db.execute(query)
         if len(result.fetchall()) > 0:
-            query = "delete from requests where ride_id='{}' and user_id='{}'"\
-                .format(ride_id, user_id[0])
+            query = "delete from requests where req_id='{}' and user_id='{}'"\
+                .format(request_id, user_id[0])
             result = db.execute(query)
             return {'message': 'Your request has been deleted.'}
         else:    
--- tests/testCancelRequest.py
+++ tests/testCancelRequest.py
@@ -29,7 +29,7 @@ class RidesRetrievalTests(Base):
 
     def test_user_can_cancel_request_for_a_ride(self):
         """test user can cancel thier request for a ride."""
-        response = self.app.delete('api/v1/users/rides/1/requests',
+        response = self.app.delete('api/v1/users/rides/requests/1',
                                  content_type='application/json',
                                  headers=self.headers_for_passenger)
         response_data = json.loads(response.get_data().decode('utf-8'))
@@ -39,7 +39,7 @@ class RidesRetrievalTests(Base):
 
     def test_user_cannot_cancel_non_exsting_request_for_a_ride(self):
         """test user cannot cancel a non-existing request for a ride offer."""
-        response = self.app.delete('api/v1/users/rides/-1/requests',
+        response = self.app.delete('api/v1/users/rides/requests/-1',
                                  content_type='application/json',
                                  headers=self.headers_for_passenger)
         response_data = json.loads(response.get_data().decode('utf-8'))
